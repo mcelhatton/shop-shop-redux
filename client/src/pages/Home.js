61f58101bf8c6f9a486e58f175716fe95218b9db
Colin McElhatton <mcelhatton@Colins-MacBook-Pro-2.local>
2022-07-28 12:35:47 -0500

    saving working cart code

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,21 +1,16 @@
 import React from "react";
-import ReactDom from "react-dom";
 import { Provider } from "react-redux";
 import store from "../../src/store";
-import ProductList from "../components/ProductList";
-import CategoryMenu from "../components/CategoryMenu";
 import Board from "../components/Board";
 import Cart from "../components/Cart";
 
 const Home = () => {
   return (
     <div className="container">
-      {/* <CategoryMenu /> */}
       <Provider store={store}>
         <Board />
       </Provider>,
-      {/* <ProductList />
-      <Cart /> */}
+      <Cart />
     </div>
   );
 };
