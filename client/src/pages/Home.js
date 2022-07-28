e60d7267a594081737a24357a06570c6cdaea064
Colin McElhatton <mcelhatton@Colins-MacBook-Pro-2.local>
2022-07-28 11:56:46 -0500

    saving working board ui code

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,21 @@
 import React from "react";
+import ReactDom from "react-dom";
+import { Provider } from "react-redux";
+import store from "../../src/store";
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
+import Board from "../components/Board";
 import Cart from "../components/Cart";
 
 const Home = () => {
   return (
     <div className="container">
-      <CategoryMenu />
-      <ProductList />
-      <Cart />
+      {/* <CategoryMenu /> */}
+      <Provider store={store}>
+        <Board />
+      </Provider>,
+      {/* <ProductList />
+      <Cart /> */}
     </div>
   );
 };
